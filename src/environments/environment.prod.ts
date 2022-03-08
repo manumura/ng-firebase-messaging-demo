70c4fc74f3e42cf69650d17217d9911dfec66c9c
manumura <manumura@yahoo.com>
2022-03-08 14:49:07 +0700

    update env variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDWxsJMFccGa8dLBjYzUWZ62LEkV9amk_s',
-    authDomain: 'ng-firebase-messaging-demo.firebaseapp.com',
-    projectId: 'ng-firebase-messaging-demo',
-    storageBucket: 'ng-firebase-messaging-demo.appspot.com',
-    messagingSenderId: '545326615823',
-    appId: '1:545326615823:web:d06c70736af1b44646c27a',
-    measurementId: 'G-N1XGBTW9V0',
+    apiKey: 'YOUR_API_KEY',
+    authDomain: 'YOUR_AUTH_DOMAIN',
+    projectId: 'YOUR_PROJECT_ID',
+    storageBucket: 'YOUR_STORAGE_BUCKET',
+    messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+    appId: 'YOUR_APP_ID',
+    measurementId: 'YOUR_MEASUREMENT_ID',
     vapidKey:
-      'BJoq00U9ZlAfdcYVc47EwbEOn0pdyy5qGTUmuQDwO1spnLfa_t_2LULIi-ofQbIszdZ-eQ8GpUBtUoU1IrCb0x0',
+      'YOUR_VAPID_KEY',
   },
 };
